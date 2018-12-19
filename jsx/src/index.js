b80bc5ca7b677dac04d28a1b19321bf232dc9ade
Neel Patel <neel136846@gmail.com>
2018-12-19 11:52:32 -0600

    Created a simple component containing text

---
+++ jsx/src/index.js
@@ -0,0 +1,16 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+// Create a React Component
+const App = () => {
+    return <div>Hi there!</div>
+};
+
+// Take the React Component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
